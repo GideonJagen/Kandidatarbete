e6e12ddb68778ce4c0401c8356d470cd37c086b9
linneahallin <hallinl@student.chalmers.se>
2021-03-12 14:39:38 +0100

    Delete redundant code

--- components/age/age.py
+++ components/age/age.py
@@ -2,21 +2,6 @@ import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 
-dcc.RangeSlider(
-    min=0,
-    max=10,
-    step=None,
-    marks={
-        0: '0 °F',
-        3: '3 °F',
-        5: '5 °F',
-        7.65: '7.65 °F',
-        10: '10 °F'
-    },
-    value=[3, 7.65]
-)
-
-
 def age_widget():
     widget = html.Div([
         dcc.RangeSlider(
