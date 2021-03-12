f123fd71ea8ec5754fad8290eef4755b3fb18460
linneahallin <hallinl@student.chalmers.se>
2021-03-12 14:20:53 +0100

    Change radioboxes to rangeslider for age

--- components/age/age.py
+++ components/age/age.py
@@ -2,26 +2,34 @@ import dash_core_components as dcc
 import dash_bootstrap_components as dbc
 import dash_html_components as html
 
+dcc.RangeSlider(
+    min=0,
+    max=10,
+    step=None,
+    marks={
+        0: '0 °F',
+        3: '3 °F',
+        5: '5 °F',
+        7.65: '7.65 °F',
+        10: '10 °F'
+    },
+    value=[3, 7.65]
+)
+
 
 def age_widget():
     widget = html.Div([
-        dcc.RadioItems(
-            id = 'age_radio',
-            options=[
-                {'label': '<16', 'value': 'off'},
-                {'label': '≥16', 'value': 'on'}
-            ],
-            value = 'off'
-        ),
-
-        #uncheck when not visible?
-        html.Div([
-            dcc.Checklist(
-            id = 'eighty',
-            options = [{'label': 'Inkludera patienter över 80', 'value': 'eighty'}],
-            labelStyle={'display': 'inline-block'}
-            )
-        ], style= {'display': 'block'}
+        dcc.RangeSlider(
+            min=0,
+            max=10,
+            step=None,
+            marks={
+                0: '0',
+                2: '16',
+                8: '80',
+                10: 'max'
+            },
+            value=[0,10]
         )
         ])
     return widget
--- components/age/age_callback.py
+++
@@ -1,9 +0,0 @@
-@app.callback(
-   Output(component_id='eighty', component_property='style'),
-   [Input(component_id='age_radio', component_property='value')])
-
-def show_hide_element(visibility_state):
-    if visibility_state == 'on':
-        return {'display': 'block'}
-    if visibility_state == 'off':
-        return {'display': 'none'}
