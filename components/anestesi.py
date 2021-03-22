52fd4a72d517687c04781da59e2f1fd2684ab8d0
Raoul Kent <raoulkent@gmail.com>
2021-03-22 14:47:18 +0100

    Refactor anestesi widget to use dash-bootstrap-components

--- components/anestesi.py
+++ components/anestesi.py
@@ -9,41 +9,18 @@ class AnestesiWidget:
 
     @staticmethod
     def anestesi_widget():
-        widget = html.Div(
-            id="anestesi",
+        widget = dbc.FormGroup(
             children=[
-                html.H4(
-                    id="anestesi_h4",
-                    children="Anestesibedömning",
-                    style={
-                        # "borderWidth": "1px",
-                        # "borderStyle": "solid",
-                        # "margin": "0px",
-                    },
-                ),
-                dcc.Checklist(
+                dbc.Label("Anestesibedömning"),
+                dbc.Checklist(
                     id="anestesi_checklist",
                     options=[
                         {"label": "Ej klar", "value": "ek"},
                         {"label": "Påbörjad", "value": "pb"},
                         {"label": "Klar", "value": "klar"},
                     ],
-                    labelStyle={"display": "inline-block"},
-                    style={
-                        "width": "100%",
-                        # "borderWidth": "1px",
-                        # "borderStyle": "solid",
-                        # "margin": "0px",
-                    },
                 ),
-            ],
-            style={
-                "width": "30%",
-                # "borderWidth": "1px",
-                # "borderStyle": "solid",
-                # "textAlign": "center",
-                # "margin": "5px",
-            },
+            ]
         )
         return widget
 
