df3c445e5d7890b2b02663fec9cfffc1141ed858
linneahallin <hallinl@student.chalmers.se>
2021-05-02 20:50:05 +0200

    Disable stat_code and add priority

--- components/priority.py
+++ components/priority.py
@@ -4,33 +4,33 @@ import dash_html_components as html
 from dash.dependencies import Input, Output
 
 
-class StatisticsCode:
+class Priority:
     STANDARD_VALUE = []
-    SELECT_STAT_CODES = "Välj statistikkoder"
+    SELECT_STAT_CODES = "Välj prioriteter"
 
     @staticmethod
     def get_component():
         widget = dbc.FormGroup(
-            id="statistics_code_widget",
+            id="priority_widget",
             children=[
                 dbc.Label(
-                    "Statistikkod",
+                    "Prioritet",
                     className="label col-form-label-lg font-weight-bold mb-n4 pd-n4",
                 ),
                 html.Hr(style={"margin-top": 0, "margin-bottom": 10}),
                 dbc.RadioItems(
-                    id="statistics_radio_items",
+                    id="priority_radio_items",
                     value="Visa alla",
                     options=[
                         {"label": "Visa alla", "value": "Visa alla"},
-                        {"label": "Välj: ", "value": "Välj", "disabled": True},
+                        {"label": "Välj: ", "value": "Välj"},
                     ],
                 ),
                 dbc.Collapse(
-                    id="statistics_collapse",
+                    id="priority_collapse",
                     is_open=False,
                     children=[
-                        StatisticsCode._statistics_code_checklist(),
+                        Priority._priority_checklist(),
                     ],
                 ),
             ],
@@ -38,20 +38,16 @@ class StatisticsCode:
         return widget
 
     @staticmethod
-    def _statistics_code_checklist():
+    def _priority_checklist():
         codes = [
             {"label": "30 dagar", "value": "30 dagar"},
             {"label": "60 dagar", "value": "60 dagar"},
             {"label": "90 dagar", "value": "90 dagar"},
-            {"label": "6 månader", "value": "6 månader"},
-            {"label": "9 månader", "value": "9 månader"},
-            {"label": "1 år", "value": "1 år"},
-            {"label": ">1 år", "value": ">1 år"},
         ]
 
         # TODO: Replace this dropdown with a dbc component, if there's a suitable replacement
         checklist = dbc.Checklist(
-            id="statistics_checklist",
+            id="priority_checklist",
             options=codes,
             labelStyle={"display": "inline-block"},
         )
@@ -59,23 +55,23 @@ class StatisticsCode:
 
     @staticmethod
     def add_callback(app):
-        app = StatisticsCode._add_callback(app)
+        app = Priority._add_callback(app)
 
         @app.callback(
-            Output(component_id="statistics_checklist", component_property="value"),
-            Output(component_id="statistics_radio_items", component_property="value"),
+            Output(component_id="priority_checklist", component_property="value"),
+            Output(component_id="priority_radio_items", component_property="value"),
             Input(component_id="reset_filter_button", component_property="n_clicks"),
         )
         def reset_component(n_clicks):
-            return StatisticsCode.STANDARD_VALUE, "Visa alla"
+            return Priority.STANDARD_VALUE, "Visa alla"
 
         return app
 
     @staticmethod
     def _add_callback(app):
         @app.callback(
-            Output(component_id="statistics_collapse", component_property="is_open"),
-            Input(component_id="statistics_radio_items", component_property="value"),
+            Output(component_id="priority_collapse", component_property="is_open"),
+            Input(component_id="priority_radio_items", component_property="value"),
             Input(component_id="reset_filter_button", component_property="n_clicks"),
         )
         def collapse(value, reset):
@@ -89,17 +85,15 @@ class StatisticsCode:
     @staticmethod
     def add_str_callback(app):
         @app.callback(
-            Output(
-                component_id="active_statistics_code", component_property="children"
-            ),
-            Input(component_id="statistics_checklist", component_property="value"),
-            Input(component_id="statistics_radio_items", component_property="value"),
+            Output(component_id="active_priority", component_property="children"),
+            Input(component_id="priority_checklist", component_property="value"),
+            Input(component_id="priority_radio_items", component_property="value"),
         )
         def update_str(value_cl, value_ri):
-            return StatisticsCode.value_to_string(value_cl, value_ri)
+            return Priority.value_to_string(value_cl, value_ri)
 
         return app
 
     @staticmethod
     def value_to_string(value_cl, value_ri):
-        return f"Statistikkod: {' ,'.join([str(s) for s in value_cl]) if 0 < len(value_cl) < 7 and value_ri == 'Välj' else 'Alla'}"
+        return f"Prioritet: {' ,'.join([str(s) for s in value_cl]) if 0 < len(value_cl) < 7 and value_ri == 'Välj' else 'Alla'}"
